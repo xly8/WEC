a9e8ba32697711e4f968ac861bfb57f631dff8dc
LEYANG XING <lxing24@uwo.ca>
2024-01-12 18:54:46 -0500

    fix map bug

--- wec/src/app/components/Map.tsx
+++ wec/src/app/components/Map.tsx
@@ -1,33 +1,52 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+'use client';
 
-const Map = () => {
-  const router = useRouter();
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Set the path for the Leaflet marker images
+const iconRetinaUrl = '/marker-icon-2x.png';
+const iconUrl = '/marker-icon.png';
+const shadowUrl = '/marker-shadow.png';
+
+// Create a new Leaflet icon
+const defaultIcon = new L.Icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+// Define a type for the item structure
+type MapItem = {
+  id: number;
+  position: [number, number]; // Tuple for latitude and longitude
+  label: string;
+};
+
+const MyMapComponent: React.FC = () => {
+  const [items, setItems] = useState<MapItem[]>([
+    { id: 1, position: [51.505, -0.09], label: 'Marker 1' },
+    // ... more items
+  ]);
 
   return (
-    <>
-      <h1>WEC</h1>
-      <h3>Disaster Map</h3>
-      <div id="map">
-        <MapContainer
-          center={[51.505, -0.09]}
-          zoom={13}
-          scrollWheelZoom={false}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      </div>
-    </>
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '400px', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {items.map((item: MapItem) => (
+        <Marker key={item.id} position={item.position} icon={defaultIcon}>
+          <Popup>{item.label}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 };
 
-export default Map;
+export default MyMapComponent;
